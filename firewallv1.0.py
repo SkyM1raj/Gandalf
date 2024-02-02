cfa42c62956d56d086b1799283be573a019f60f3
BlackBird63030 <141164867+BlackBird63030@users.noreply.github.com>
2024-02-02 16:03:58 +0100

    Update firewallv1.0.py

--- firewallv1.0.py
+++ firewallv1.0.py
@@ -210,6 +210,97 @@ def firewall(pkt):
     # print("Packet from %s accepted and forwarded to IPTABLES" %(sca.src)) #commented coz its annoying
     pkt.accept()
 
+class pywall:
+    def __init__(self, iface=None, timeout=15):
+        self.iface = iface
+        self.timeout = timeout
+        self.arp_spoofing_detected = None
+
+    def get_mac_address(self, target):
+        """
+        Get mac address of target
+        """
+        result = srp(
+            Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=target), timeout=3, verbose=0
+        )[0]
+        result = [received.hwsrc for sent, received in result]
+
+    def arp_spoofing_detection(self):
+        """
+        Detect arp spoofing
+        """
+
+        def __control(packet):
+            return self.arp_spoofing_detected is not None
+
+        def __process_sniffed_packet(packet):
+            if packet.haslayer(ARP) and packet[ARP].op == 2:
+                real_mac = self.get_mac_address(packet[ARP].psrc)
+                response_mac = packet[ARP].hwsrc
+                self.arp_spoofing_detected = real_mac != response_mac
+
+        sniff(
+            iface=self.iface,
+            store=False,
+            stop_filter=__control,
+            prn=__process_sniffed_packet,
+            timeout=self.timeout,
+        )
+
+        return self.arp_spoofing_detected
+
+    def control(self):
+        """
+        Main function
+        """
+
+        return self.arp_spoofing_detection()
+
+
+def arguments():
+    """
+    Main function
+    """
+
+    parser = ArgumentParser()
+    parser.add_argument("-i", "--iface", type=str, help="Interface")
+    parser.add_argument("-t", "--timeout", type=int, help="Timeout")
+
+    args = parser.parse_args()
+
+    the_pywall = pywall()
+
+    if args.iface is not None:
+        the_pywall.iface = args.iface
+    if args.timeout is not None:
+        the_pywall.timeout = args.timeout
+
+    print(the_pywall.control())
+
+def handle_client(client_socket, client_addr):
+    client_ip, _ = client_addr
+
+    try:
+        # Ajout de la détection ARP spoofing
+        arp_detector = pywall(iface="your_network_interface", timeout=15)
+        arp_spoofing_detected = arp_detector.control()
+
+        if is_locked and not arp_spoofing_detected:
+            response = b"Pare-feu verrouillé. Veuillez entrer le mot de passe admin.\n"
+        elif not is_locked and arp_spoofing_detected:
+            response = b"Détection ARP spoofing. Veuillez entrer le mot de passe admin.\n"
+        elif is_locked and arp_spoofing_detected:
+            response = b"Pare-feu verrouillé et détection ARP spoofing. Veuillez entrer le mot de passe admin.\n"
+        else:
+            # Ajustement de la logique pour imprimer un message lorsque tout va bien
+            print("Tout va bien. Connexion autorisée par le pare-feu.")
+            response = b"Connexion autorisée par le pare-feu.\n"
+
+        client_socket.send(response)
+    finally:
+        client_socket.close()
+        reset_connection_count(client_ip)
+
 def start_firewall_and_server():
     nfqueue = NetfilterQueue()
     nfqueue.bind(1, firewall)
